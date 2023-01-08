31abe05f81c3d6a6d7dbd8ee48f33b74258e0bcb
Bill Wen <gang.wen@gmail.com>
2023-01-08 19:31:55 +0800

    2-3 http请求的结构

--- uaa/src/main/java/com/guludoc/learning/u3app/uaa/rest/UserResource.java
+++ uaa/src/main/java/com/guludoc/learning/u3app/uaa/rest/UserResource.java
@@ -1,5 +1,6 @@
 package com.guludoc.learning.u3app.uaa.rest;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,5 +15,22 @@ public class UserResource {
     @PostMapping("/greeting")
     public String makeGreeting(@RequestParam String name) {
         return "Hello " + name;
+    }
+
+    @PutMapping("/greeting/{name}")
+    public String putGreeting(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+    @PostMapping("/greetingwithjson")
+    public String makeGreeting(@RequestBody Profile profile) {
+        return "Hello " + profile.getGender();
+    }
+
+    @Data
+    static class Profile {
+        private String gender;
+
+        private String idNo;
     }
 }
