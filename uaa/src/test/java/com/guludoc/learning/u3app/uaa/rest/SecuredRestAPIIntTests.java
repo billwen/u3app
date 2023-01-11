a5d01c312e2fa56c8f838b34ae4a832cbe8903a9
Bill Wen <gang.wen@gmail.com>
2023-01-11 21:34:09 +0800

    4-7 自动化测试

---
+++ uaa/src/test/java/com/guludoc/learning/u3app/uaa/rest/SecuredRestAPIIntTests.java
@@ -0,0 +1,45 @@
+package com.guludoc.learning.u3app.uaa.rest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@SpringBootTest
+@ActiveProfiles("dev")
+public class SecuredRestAPIIntTests {
+
+    private final WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser
+    @Test
+    public void givenAuthRequest_shouldSuccessWith200() throws Exception {
+        mockMvc.perform(
+                    get("/api/principal")
+                )
+                .andExpect(status().isOk());
+    }
+}
