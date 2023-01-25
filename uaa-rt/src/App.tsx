fc688df02e95f7fb82e59de23099a40b94419937
Bill Wen <gang.wen@gmail.com>
2023-01-25 11:28:46 +0800

    05 Switching route elements with routes

--- uaa-rt/src/App.tsx
+++ uaa-rt/src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { css } from '@emotion/css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Products from "./Products/Products";
+import Admin from "./Admin/Admin";
 
-import { BrowserRouter as Router} from 'react-router-dom';
-
+const AppStyles = css`
+  margin: 50px auto;
+  width: 380px;
+  
+  .Container {
+    background: #1d1e26;
+    border: 4px solid #9580ff;
+    border-radius: 6px;
+    padding: 25px;
+  }
+`;
 const App = () => {
   return (
-      <div>
-        <Router>Hello</Router>
+      <div className={AppStyles}>
+        <Router>
+            <div className="Container">
+                <Routes>
+                    <Route path="/" element={ <Products /> } />
+                    <Route path="/admin" element={ <Admin /> } />
+                </Routes>
+            </div>
+        </Router>
       </div>
   );
 }
---
+++ uaa-rt/src/Products/Products.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { css } from '@emotion/css'
+
+const ProductsStyles = css`
+display: flex;
+flex-direction: column;
+
+.Logo {
+    width: 125px;
+    margin: 0 auto 25px;
+}
+`;
+
+const Products = () => {
+    return (
+        <div className={ProductsStyles}>
+            <img src={process.env.PUBLIC_URL + '/assets/img/logo.svg'} alt="Ultimate Burgers" className="Logo"/>
+            Products
+        </div>
+    );
+}
+
+export default Products;
