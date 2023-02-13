c4306736ac607dc5dd0ffbdf70441510d39c7f80
Gang Wen <gang.wen@gmail.com>
2023-02-13 21:20:27 +0100

    20230213

---
+++ react-github/src/Auth/Login.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+    const client_id = process.env.REACT_APP_CLIENT_ID!;
+    const client_secret = process.env.REACT_APP_CLIENT_SECRET!;
+    const github_url_authorize = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=user%20read:org%20public_repo%20admin:enterprise&state=abc`;
+
+    const onLogin = () => {
+        window.location.href = github_url_authorize;
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <button type="button" onClick={onLogin}>Github</button>
+        </div>
+    );
+}
+
+export default Login;
